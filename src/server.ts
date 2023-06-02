f17bb5d94514c9bfb71a84176883de4af349ba83
jobair-talha <talhajobair10@gmail.com>
2023-06-03 01:50:27 +0600

    add cors and epress middleware

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    console.log(`Database connection successfully!`);
+
+    app.listen(5000, () => {
+      console.log(`Application listening on port 5000`);
+    });
+  } catch (error) {
+    console.log(`Failed to connect database`, error);
+  }
+}
+
+main();
