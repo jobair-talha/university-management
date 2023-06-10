934348d38454c6cba2b6ba8b61d20fd8fbb330b1
jobair-talha <talhajobair10@gmail.com>
2023-06-10 12:51:52 +0600

    setup winston winston-daily-rotate-file logger

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import { errorLogger, logger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
 
-    console.log(`Database connection successfully!`)
+    logger.info(`Database connect successfully!`)
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port 5000`)
+      logger.info(`Application listening on http://localhost/5000`)
     })
   } catch (error) {
-    console.log(`Failed to connect database`, error)
+    errorLogger.error(`Failed to connect database`, error)
   }
 }
 
